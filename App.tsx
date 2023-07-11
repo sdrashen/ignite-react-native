5b19fbe9501de0c1717035e29d7872a81df90f0f
Sabrina Drashen <sdrashen@gmail.com>
2023-07-11 19:47:05 -0300

    style: inline and stylesheet

--- App.tsx
+++ App.tsx
@@ -1,12 +1,33 @@
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 export default function App() {
   return (
-    <View>
-      <Text>React Native</Text>
-      <Text>For mobile development</Text>
+    <View style={styles.container}>
+      <Text style={{
+        color: '#e78ed6',
+        fontSize: 24,
+        fontWeight: 'bold'
+      }}>
+        React Native
+      </Text>
+
+      <Text
+        style={{
+          color: '#e78ed6',
+          fontSize: 20
+        }}
+      >For mobile development
+      </Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#2f292b",
+    padding: 24
+  }
+})
 
 
